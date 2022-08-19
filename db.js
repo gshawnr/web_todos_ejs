b3fd33075116c658e71f855c889669e8d78dca25
gsr <gshawnr@gmail.com>
2022-08-18 17:46:53 -0700

    add dotenv

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const db = require("mongoose");
+require("dotenv").config();
+console.log(process.env);
 
 const url = "mongodb://localhost:27017/TodoDB";
-db.connect(url);
+const onlineUrl = `mongodb+srv://${MONGO_USER}:${MONGO_KEY}@gsr-mongo-cluster.kkuknqw.mongodb.net/TodoDB`;
+
+db.connect(onlineUrl);
 
 exports.createModel = (inputName, inputSchema) => {
   const modelSchema = new db.Schema(inputSchema);
