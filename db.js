5beebfc375e6eccbec130727303777a5012148d6
gsr <gshawnr@gmail.com>
2022-08-18 17:49:29 -0700

    test cloud db with secrets

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const db = require("mongoose");
 require("dotenv").config();
-console.log(process.env);
 
 const url = "mongodb://localhost:27017/TodoDB";
-const onlineUrl = `mongodb+srv://${MONGO_USER}:${MONGO_KEY}@gsr-mongo-cluster.kkuknqw.mongodb.net/TodoDB`;
+const onlineUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_KEY}@gsr-mongo-cluster.kkuknqw.mongodb.net/TodoDB`;
 
 db.connect(onlineUrl);
 
